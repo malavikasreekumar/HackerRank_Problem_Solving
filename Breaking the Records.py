6f53549370b54046610c7c4a4e978761ab68fc66
Malavika Sreekumar <35482521+malavikasreekumar@users.noreply.github.com>
2019-05-24 22:15:37 +0530

    Create Breaking the Records.py

---
+++ Breaking the Records.py
@@ -0,0 +1,13 @@
+def breakingRecords(scores):
+    maxi=scores[0]
+    mini=scores[0]
+    maxcount =0
+    mincount=0
+    for i in range(len(scores)):
+        if(scores[i]>maxi):
+            maxi = scores[i]
+            maxcount+=1
+        if(scores[i]<mini):
+            mini = scores[i]
+            mincount+=1
+    return [maxcount, mincount]
