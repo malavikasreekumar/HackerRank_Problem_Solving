f4c7831ae926323c12a4051a08bb2c5747756298
Malavika Sreekumar <35482521+malavikasreekumar@users.noreply.github.com>
2019-05-31 19:29:47 +0530

    Create Angry Professor.py

---
+++ Angry Professor.py
@@ -0,0 +1,10 @@
+def angryProfessor(k, a):
+    cnt=0
+    for i in range(0,len(a)):
+        if a[i]<=0:
+            cnt=cnt+1
+    
+    if cnt>=k:
+        return "NO"
+    else:
+        return "YES"
