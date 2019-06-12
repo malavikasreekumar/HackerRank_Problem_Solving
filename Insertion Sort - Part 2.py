9d73cb38e31c4b5b6d8373e5e5de8d56c55e30d8
Malavika Sreekumar <35482521+malavikasreekumar@users.noreply.github.com>
2019-06-12 11:59:20 +0530

    Create Insertion Sort - Part 2.py

---
+++ Insertion Sort - Part 2.py
@@ -0,0 +1,13 @@
+def insertionSort2(n, arr):
+    for i in range(1, len(arr)): 
+  
+        key = arr[i] 
+  
+        
+        j = i-1
+        while j >=0 and key < arr[j] : 
+                arr[j+1] = arr[j] 
+                j -= 1
+        arr[j+1] = key 
+        
+        print (' '.join(str(j) for j in arr))
